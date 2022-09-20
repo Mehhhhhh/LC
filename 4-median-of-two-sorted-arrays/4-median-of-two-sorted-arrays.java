7ff7792128fbc20ef480bf79186996b49aee4daf
Xin Lu <bruyereseb@gmail.com>
2022-09-20 22:14:56 +0100

    Time: 4 ms (73.65%), Space: 50.4 MB (20.85%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -14,25 +14,23 @@ class Solution {
         int n = nums2.length;
         if(m > n) { return findKth(nums2, nums1, k);}
         
-        if (m == 0) return nums2[k-1];
-        
-        int l = 0, r = m-1;
-        while(l <= r) {
+        int l = -1, r = m ;
+        while(l < r) {
             int mid1 = l + (r - l) / 2;
-            int mid2 = k - mid1 - 2;
+            int mid2 = k - mid1 -2;
             int left1 = mid1 >= 0? nums1[mid1]: Integer.MIN_VALUE;
             int left2 = mid2 >= 0? nums2[mid2]: Integer.MIN_VALUE;
             int right1 = mid1 + 1 < m ? nums1[mid1+1] : Integer.MAX_VALUE;
-            int right2= mid2+ 1 < n ? nums2[mid2+1] : Integer.MAX_VALUE;
-            if (left1 > right2) {
-                r = mid1 - 1;
-            } else if (left2 > right1) {
-                l = mid1+1;
-            } else {
+            int right2= mid2  +1 < n ? nums2[mid2+1] : Integer.MAX_VALUE;
+            if (left1 <= right2 && left2 <= right1) {
                 return Math.max(left1, left2);
-            }
+            } else if (left1 > right2) {
+                r = mid1;
+            } else {l = mid1 + 1;}
         }
-        return nums2[k-1];
+        if (r == 0) {return nums2[k-1];}
+        if (r == m) {return nums1[k-1];}
+        return -1;
     }
     
 }
